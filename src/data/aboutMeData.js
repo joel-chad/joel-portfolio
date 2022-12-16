e77ab7060b656b0a2a1185c99d0a52a44eb90392
joel-chad <joelchadambuka@gmail.com>
2022-12-16 02:08:59 +0200

    push

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,9 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'My name is Joel Chadambuka. I am a 24 year old UI/UX developer based in Harare, Zimbabwe. I recently graduated from Harare Institute of Technology with a BTech Honours Degree in Software Engineering. I work mostly with HTML, CSS and Javascript. I have experience in React and I have used CSS frameworks like Bootstrap and Tailwind CSS for over 3 years. I have worked on various web applications and websites over the course of my career so far. I am looking to gain more experience in frontend development especially in areas I have not worked in as much like mobile app development. I am willing to take short to medium term projects and full time employment.'	},
+	{
+		id: 2,
+		bio: 'Feel free to contact me if you feel like I could be of service to your organisation. I look forward to hearing from you',
+	},
+];
